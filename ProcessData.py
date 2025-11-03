b00e5b5dcd694184a0abb2c9a56a5c8335914be0
treyruge15 <treyruge15@gmail.com>
2025-11-03 02:05:36 +0000

    Lab 8 - Trey Ruge

--- ProcessData.py
+++ ProcessData.py
@@ -5,6 +5,16 @@
 
 import random
 
+def makeID(first,last, idNum):
+    idlen = len(idNum)
+
+    while len(last) < 5:
+      last = last + "X"
+
+    id = first[0] + last +idNum[idlen -3: ]
+
+    return id
+
 def main():
 
   #Open the files we will be using
@@ -12,6 +22,30 @@ def main():
   outFile = open("StudentList.csv", 'w')
 
   #Process each line of the input file and output to the CSV file
+
+  for line in inFile:
+    data = line.split()
+    first = data[0]
+    last = data[1]
+    idNum = data[3]
+    
+    year = {
+      "Freshman": "FR",
+      "Sophomore": "SO",
+      "Junior": "JR",
+      "Senior": "SR"
+    }
+
+    year1 = year[data[5]]
+    major = " ".join(data[6:])
+    major3 = major[:3]
+
+    student_id = makeID(data[0], data[1], data[3])
+    output = last + "," + first + "," + student_id + "," + major3 + "-" + year1 + "\n"
+    outFile.write(output)
+
+  #print(student_id)
+
 
 
 
